528dde7ebce5e866b705fa95dcb27bb3c0296cf2
omoralessg <olga.morales@globant.com>
2020-02-07 18:00:24 -0600

    network state

---
+++ app/src/main/java/com/example/androidfundamental/network/NetworkActivity.kt
@@ -0,0 +1,42 @@
+package com.example.androidfundamental.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidfundamental.R
+import kotlinx.android.synthetic.main.activity_networking.*
+
+class NetworkActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_networking)
+        if (isInternetAvailable(this)) {
+            txt_internet.text = "internet avaiable"
+
+        } else {
+            txt_internet.text = "internet unavaiable"
+        }
+    }
+
+    private fun isInternetAvailable(context: Context): Boolean {
+        var result = false
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = connectivityManager.activeNetwork ?: return false
+            val actNw =
+                connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+            result = when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        }
+        return result
+    }
+}
